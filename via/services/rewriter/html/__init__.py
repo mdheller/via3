8367fe99f7f0920c8077731c68b561f6088e6eda
Jon Betts <jon.betts@hypothes.is>
2020-05-22 14:45:50 +0100

    Added LXML streaming parser. Converted HTMLParser to streaming

--- via/services/rewriter/html/__init__.py
+++ via/services/rewriter/html/__init__.py
@@ -1,10 +1,12 @@
-from via.services.rewriter.html.htmlparser import HTMLParserRewriter
+from via.services.rewriter.html.htmlparser_streaming import HTMLParserRewriter
 from via.services.rewriter.html.lxml import LXMLRewriter
+from via.services.rewriter.html.lxml_streaming import LXMLStreamingRewriter
 from via.services.rewriter.html.null import NullRewriter
 
 HTML_REWRITERS = {
     "htmlparser": HTMLParserRewriter,
     "lxml": LXMLRewriter,
+    "lxml_stream": LXMLStreamingRewriter,
     "null": NullRewriter,
     None: LXMLRewriter,
 }
--- via/services/rewriter/html/abstract.py
+++ via/services/rewriter/html/abstract.py
@@ -1,4 +1,5 @@
 import os
+from html import escape
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -28,3 +29,16 @@ class AbstractHTMLRewriter(AbstractRewriter):
             h_embed_url=os.environ.get("H_EMBED_URL", "https://hypothes.is/embed.js"),
             hypothesis_config=self._h_config,
         )
+
+    def get_page_inserts(self, doc_url):
+        if not self.inject_client:
+            return {}
+
+        # base_url = self.url_rewriter.rewrite_html(doc_url)
+        embed = self._get_client_embed()
+
+        return {
+            # "head_top": f'\n<link rel="canonical" href="{escape(doc_url)}">\n<base href="{escape(base_url)}">\n',
+            "head_top": f'\n<link rel="canonical" href="{escape(doc_url)}">\n<base href="{escape(doc_url)}">\n',
+            "head_bottom": f'\n<script type="text/javascript">{embed}</script>\n',
+        }
