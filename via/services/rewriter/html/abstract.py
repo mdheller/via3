dab9e674adac6a6da6a5e42d4c65143f3cd9af58
Jon Betts <jon.betts@hypothes.is>
2020-05-22 17:09:15 +0100

    Adding a placeholder for clientside JS injection

--- via/services/rewriter/html/abstract.py
+++ via/services/rewriter/html/abstract.py
@@ -1,22 +1,26 @@
+import json
 import os
 from html import escape
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from via.services.rewriter.interface import AbstractRewriter
+from via.services.rewriter.ruleset import Rules
 
 
 class AbstractHTMLRewriter(AbstractRewriter):
-    # Things our children do
     inject_client = True
+    inject_js_rewriter = True
 
-    def __init__(self, url_rewriter, h_config):
+    def __init__(self, url_rewriter, h_config, static_url):
         """
         :param static_url: The base URL for our transparent proxying
         """
         super().__init__(url_rewriter)
 
         self._h_config = h_config
+        self._static_url = static_url
+
         self._jinja_env = Environment(
             loader=PackageLoader("via", "templates"),
             autoescape=select_autoescape(["html", "xml"]),
@@ -37,8 +41,25 @@ class AbstractHTMLRewriter(AbstractRewriter):
         # base_url = self.url_rewriter.rewrite_html(doc_url)
         embed = self._get_client_embed()
 
+        head_top = f'\n<link rel="canonical" href="{escape(doc_url)}">\n<base href="{escape(doc_url)}">\n'
+
+        if self.inject_js_rewriter:
+            head_top += self._client_side_rewriter_inject(doc_url)
+
         return {
-            # "head_top": f'\n<link rel="canonical" href="{escape(doc_url)}">\n<base href="{escape(base_url)}">\n',
-            "head_top": f'\n<link rel="canonical" href="{escape(doc_url)}">\n<base href="{escape(doc_url)}">\n',
+            "head_top": head_top,
             "head_bottom": f'\n<script type="text/javascript">{embed}</script>\n',
         }
+
+    def _client_side_rewriter_inject(self, doc_url):
+        settings = {
+            "baseUrl": doc_url,
+            **self.url_rewriter.get_templates(),
+            "ruleset": Rules.js().as_list(),
+        }
+
+        return f"""<script type="text/javascript">
+            var VIA_REWRITER_SETTINGS = {json.dumps(settings)};
+        </script>
+        <script src="{self._static_url('via:static/js/rewriter.js')}">
+        """
--- via/services/rewriter/html/lxml_streaming.py
+++ via/services/rewriter/html/lxml_streaming.py
@@ -7,7 +7,7 @@ from via.services.rewriter.html.tag import TagFactory
 from via.services.timeit import timeit
 
 
-class StreamingParser:
+class ParserCallback:
     def __init__(self, tag_factory, inserts):
         self.buffer = deque()
         self.tag_factory = tag_factory
@@ -39,16 +39,28 @@ class LXMLStreamingRewriter(AbstractHTMLRewriter):
     streaming = True
 
     def rewrite(self, doc):
-        echo = StreamingParser(
+        callback = ParserCallback(
             TagFactory(self.url_rewriter), inserts=self.get_page_inserts(doc.url)
         )
-        parser = HTMLParser(target=echo)
+        parser = HTMLParser(target=callback)
+
+        lines_written = 0
+        lines_read = 0
 
         with timeit("streaming HTML rewrite time"):
             # TODO! How are you supposed to get the real one?
             yield b"<!DOCTYPE html>\n"
 
             for line in doc.original.iter_lines():
+                lines_read += 1
+
                 parser.feed(line)
-                while echo.buffer:
-                    yield echo.buffer.popleft().encode("utf-8")
+                while callback.buffer:
+                    lines_written += 1
+                    yield callback.buffer.popleft().encode("utf-8")
+
+            while callback.buffer:
+                lines_written += 1
+                yield callback.buffer.popleft().encode("utf-8")
+
+        print(f"LXML STREAMED: lines {lines_read} read / {lines_written} written")
