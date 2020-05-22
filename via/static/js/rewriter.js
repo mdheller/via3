925fe68920202e0d47a02c92b6b0b89b294124c4
Robert Knight <robertknight@gmail.com>
2020-05-22 17:40:34 +0100

    WIP - Proxy fetch/XHR requests
    Proxy fetch/XMLHttpRequest requests on the client through the nginx
    proxy server. This is necessary to avoid the requests failing due to
    CORS failures when a script on the proxied domain (via.hypothes.is)
    makes a request to the original domain.

--- via/static/js/rewriter.js
+++ via/static/js/rewriter.js
@@ -1,1 +1,18 @@
 /* Javascript interceptions for rewriting HTML pages */
+console.log('Initializing Via DOM API monkey-patching');
+
+function staticProxyURL(url) {
+  const absUrl = new URL(url, document.baseURI).toString();
+  const proxyUrlTemplate = VIA_REWRITER_SETTINGS.urlTemplates.PROXY_STATIC;
+  return proxyUrlTemplate.replace('__URL__', absUrl);
+}
+
+const origFetch = window.fetch;
+window.fetch = (url, ...args) => {
+  return origFetch.call(null, staticProxyURL(url), ...args);
+};
+
+const origOpen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function (method, url, ...args) {
+  return origOpen.call(this, method, staticProxyURL(url), ...args);
+};
