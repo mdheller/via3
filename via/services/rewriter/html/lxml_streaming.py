f36ab7d04be1caa634cbb1f9e0e3564a9cfe9a3f
Jon Betts <jon.betts@hypothes.is>
2020-06-03 15:32:56 +0100

    Get the right doctype for lxml_streaming

--- via/services/rewriter/html/lxml_streaming.py
+++ via/services/rewriter/html/lxml_streaming.py
@@ -1,4 +1,5 @@
 from lxml.html import HTMLParser
+from lxml.html.html5parser import HTMLParser as HTMLParser5
 
 from via.services.rewriter.html.abstract import AbstractHTMLRewriter
 from via.services.rewriter.html.tag_factory import TagFactory
@@ -7,10 +8,6 @@ from via.services.rewriter.html.tag_factory import TagFactory
 class ParserCallback:
     def __init__(self, tag_factory, inserts, buffer):
         self.buffer = buffer
-
-        # TODO! How are you supposed to get the real one?
-        buffer.add("<!DOCTYPE html>\n")
-
         self._tag_factory = tag_factory
         self._inserts = inserts
 
@@ -33,6 +30,28 @@ class ParserCallback:
     def comment(self, text):
         self.buffer.add(f"<!-- {text} -->")
 
+    def doctype(self, doc_type, public, dtd):
+        doc_type = f"<!DOCTYPE {doc_type}"
+
+        if public:
+            doc_type += f' PUBLIC "{public}"'
+
+        if dtd:
+            doc_type += f' "{dtd}"'
+
+        doc_type += ">\n"
+
+        self.buffer.add(doc_type)
+
+    def pi(self, *args, **kwargs):
+        raise NotImplementedError(f"pi {args}, {kwargs}")
+
+    def start_ns(self, *args, **kwargs):
+        raise NotImplementedError(f"start_ns {args}, {kwargs}")
+
+    def end_ns(self, *args, **kwargs):
+        raise NotImplementedError(f"end_ns {args}, {kwargs}")
+
     def close(self):
         pass
 
@@ -42,6 +61,7 @@ class LXMLStreamingRewriter(AbstractHTMLRewriter):
 
     def _get_streaming_parser(self, doc, buffer):
         return HTMLParser(
+            collect_ids=False,
             target=ParserCallback(
                 tag_factory=TagFactory(self.url_rewriter),
                 inserts=self.get_page_inserts(doc.url),
