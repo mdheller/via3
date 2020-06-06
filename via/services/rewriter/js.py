d9bb098f70bb4fb04020d030751a1c869d491be9
Robert Knight <robertknight@gmail.com>
2020-06-06 13:43:38 +0100

    Resolve additional issues with `window` and `location` patching
     - Re-export any top-level variables defined in scripts so that they are
       visible to other scripts. For example, an early script on [1] defines a top-level
       variable called `Drupal` and other scripts on the page expect that
       variable to exist.
     - Proxy the `window.window` property. This required changing the
       _target_ of the proxy to a non-`window` object because the `Proxy`
       API enforces restrictions on how read-only, non-configurable
       properties on the target object are handled.

--- via/services/rewriter/js.py
+++ via/services/rewriter/js.py
@@ -3,6 +3,15 @@ import re
 from via.services.rewriter.interface import AbstractRewriter
 
 
+def _get_exported_vars(js_source):
+    # Perform a crude scan of JS source text to extract potential names of
+    # top-level `var` identifiers. To reduce the number of non-top level variables
+    # matched, we apply a length restriction. This filters out most inner variables
+    # due to their names being shortened after minification.
+    JS_VAR_REGEX = re.compile(r"var ([a-zA-Z0-9_$]{3,})")
+    return JS_VAR_REGEX.findall(js_source)
+
+
 class JSRewriter(AbstractRewriter):
     QUOTED_URL_REGEX = re.compile(r"[\"'](https?://[^\"']+)[\"']", re.IGNORECASE)
 
@@ -34,10 +43,38 @@ class JSRewriter(AbstractRewriter):
         for find, replace in replacements:
             content = content.replace(find, replace)
 
-        content = """
+        # Wrap script content in an IIFE so we can intercept accesses to `location`
+        # and other _unforgeable_ properties which can't be monkey-patched directly by
+        # client-side JS. See https://html.spec.whatwg.org/ for a complete
+        # list of "LegacyUnforgeable" properties.
+        #
+        # An issue with this is that any variables declared at the top level are no longer
+        # visible to other scripts but instead become scoped to the IIFE function.
+        # To work around that, we perform a crude scan of the source text to find
+        # possible exported variable names and then re-export them.
+        exported_vars = _get_exported_vars(content)
+        content = (
+            """
+var exportedVars = {};
 (function (window, location) {
-""" + content + """
-}).call(viaWindowProxy, viaWindowProxy, viaWindowProxy.location)
 """
+            + content
+            + ";"
+            + ";".join(
+                [
+                    f"exportedVars['{name}']=typeof {name} !== 'undefined' ? {name} : undefined"
+                    for name in exported_vars
+                ]
+            )
+            + """
+}).call(viaWindowProxy, viaWindowProxy, viaWindowProxy.location);
+"""
+            + "".join(
+                [
+                    f"var {name}; if (exportedVars['{name}']) {{ {name} = exportedVars['{name}'] }}"
+                    for name in exported_vars
+                ]
+            )
+        )
 
         return content
