911801a1f96e1cbd1d1ce48598b165d2a1008a59
Jon Betts <jon.betts@hypothes.is>
2020-07-06 17:27:21 +0100

    Linting and tidy up pass

--- via/get_url/browser_mimic.py
+++ via/get_url/browser_mimic.py
@@ -1,3 +1,5 @@
+"""Tools for mimicking calls directly from real browsers."""
+
 import json
 import random
 import re
@@ -13,31 +15,46 @@ from via.get_url.headers import clean_headers, reorder_headers
 
 
 class BrowserMimic:
-    referer_locations = None
-    header_sets = None
-    header_fingerprints = None
-    user_agents = None
+    """Toolset for mimicking a browser."""
 
-    non_finger_printing_keys = {"Cookie", "Referer", "Host", "DNT"}
+    # The presence of these headers are so variable they are of no use in
+    # finger printing.
+    NON_FINGER_PRINTING_HEADERS = {"Cookie", "Referer", "Host", "DNT"}
 
-    UA_NUMBERS = re.compile(r"\b[0-9._]+\b")
+    UA_NUMBER_PATTERN = re.compile(r"\b[0-9._]+\b")
 
-    @classmethod
-    def init(cls):
-        cls._load_header_sets()
-        cls._analyse_header_sets()
+    _header_sets = None
+    _header_fingerprints = None
+    _user_agents = None
 
     @classmethod
-    def random_mimic(cls):
-        return random.choice(cls.header_sets.values())
+    def random_headers(cls):
+        """Get a random set of headers captured from a browser.
+
+        :return: An OrderedDict of headers to pass to 3rd parties.
+        """
+        return random.choice(cls._header_sets.values())
 
     @classmethod
     def mimic_headers(cls, headers):
-        # To avoid detection we must present the headers in the same order we
-        # received them
+        """Convert headers to ones which appear to come from a browser.
 
+        This involves:
+
+         * Cleaning headers modified or added by our tech stack (AWS, NGINX
+           etc.)
+         * Adding defaults for headers which make them appear to have come from
+           natural browsing activity
+         * Re-order the headers as they would be received from the browser, if
+           they can be matched to a browser.
+
+        If there is no `User-Agent` at all, an example set captured from a
+        browser will be returned.
+
+        :return: An OrderedDict of headers to pass to 3rd parties.
+        """
         if "User-Agent" not in headers:
-            return cls.random_mimic()
+            return cls.random_headers()
 
         headers = clean_headers(headers)
         if "Referer" not in headers:
@@ -45,26 +62,34 @@ class BrowserMimic:
 
         browser_guess = cls.identify_browser(headers, best_guess=True)
         if browser_guess:
-            headers = reorder_headers(headers, cls.header_sets[browser_guess])
+            headers = reorder_headers(headers, cls._header_sets[browser_guess])
 
         return headers
 
     @classmethod
     def identify_browser(cls, headers, best_guess=False):
+        """Try to match the given browser to our know set.
+
+        :param headers: The headers to match on
+        :param best_guess: Get a single value, rather than all equally likely
+                           matches
+        :return: A single match if `best_guess` is enabled, or a list of
+                 equally likely candidates.
+        """
         browsers, score, match_method = [], 0.0, None
 
         user_agent = headers.get("User-Agent")
         if user_agent:
             match_method = "user-agent"
             browsers, score = cls._nearest_match(
-                user_agent, cls.user_agents, comparison_fn=cls._string_match
+                user_agent, cls._user_agents, comparison_fn=cls._string_match
             )
 
         if not browsers:
             match_method = "header-set"
             browsers, score = cls._nearest_match(
                 cls._header_fingerprint(headers),
-                cls.header_fingerprints,
+                cls._header_fingerprints,
                 comparison_fn=cls._collection_match,
             )
 
@@ -76,25 +101,37 @@ class BrowserMimic:
     @classmethod
     def _header_fingerprint(cls, headers):
         return tuple(
-            sorted(
-                key for key in headers.keys() if key not in cls.non_finger_printing_keys
-            )
+            key for key in headers.keys() if key not in cls.NON_FINGER_PRINTING_HEADERS
         )
 
     @staticmethod
+    def _collection_match(target, comparison):
+        """Edit distance ratio for arbitrary collections.
+
+        This should not be used for strings (as it's slow).
+        """
+
+        diff = SequenceMatcher(a=target, b=comparison)
+        return diff.ratio()
+
+    @staticmethod
     def _string_match(target, comparison):
+        """Edit distance ratio for strings.
+
+        This should be used for strings as it's much faster than
+        _collection_match.
+        """
         dist = distance(target, comparison)
         min_length = min(len(target), len(comparison))
 
         return 1 - (dist / max(min_length, 1))
 
-    @staticmethod
-    def _collection_match(target, comparison):
-        diff = SequenceMatcher(a=target, b=comparison)
-        return diff.ratio()
-
     @classmethod
     def _nearest_match(cls, target, pairs, comparison_fn, min_ratio=0.7):
+        """Get the nearest equal matches under the comparison function.
+
+        Higher values from the comparison function are considered better.
+        """
         best_score, best_matches = min_ratio, []
 
         for comparison, browser_names in pairs.items():
@@ -112,28 +149,37 @@ class BrowserMimic:
         return best_matches, best_score
 
     @classmethod
-    def _load_header_sets(cls):
-        cls.header_sets = {}
+    def _init(cls):
+        """Load data and pre-populate required data for comparison."""
+        cls._load_header_sets()
+        cls._analyse_header_sets()
 
-        for ua_file in glob(resource_filename("via", "resource/header_sets/*.json")):
+    @classmethod
+    def _load_header_sets(cls):
+        """Read from JSON data."""
+        cls._header_sets = {}
+
+        for ua_file in glob(resource_filename("via", "resource/_header_sets/*.json")):
             with open(ua_file) as handle:
                 header_set = OrderedDict(json.load(handle))
                 browser_name = basename(ua_file).replace(".json", "")
-                cls.header_sets[browser_name] = header_set
+                cls._header_sets[browser_name] = header_set
 
     @classmethod
     def _analyse_header_sets(cls):
+        """Create look-ups for different kinds of matching."""
         header_fingerprints = defaultdict(list)
         user_agents = defaultdict(list)
 
-        for browser_name, header_set in cls.header_sets.items():
+        for browser_name, header_set in cls._header_sets.items():
             user_agents[header_set.get("User-Agent")].append(browser_name)
             header_fingerprints[cls._header_fingerprint(header_set)].append(
                 browser_name
             )
 
-        cls.header_fingerprints = dict(header_fingerprints)
-        cls.user_agents = dict(user_agents)
+        cls._header_fingerprints = dict(header_fingerprints)
+        cls._user_agents = dict(user_agents)
 
 
-BrowserMimic.init()
+# Load static data
+BrowserMimic._init()
--- via/get_url/headers.py
+++ via/get_url/headers.py
@@ -2,12 +2,15 @@
 
 from collections import OrderedDict
 
+# A mix of headers we don't want to pass on for one reason or another
 BANNED_HEADERS = {
     # Requests needs to set Host to the right thing for us
     "Host",
+    # Don't pass Cookies
+    "Cookie",
     # AWS things
     "X-Amzn-Trace-Id",
-    # AWS NGINX things
+    # AWS NGINX / NGINX things
     "X-Forwarded-Server",
     "X-Forwarded-For",
     "X-Real-Ip",
@@ -23,8 +26,11 @@ BANNED_HEADERS = {
     "Cdn-Loop",
 }
 
+# Something get incorrectly Title-cased by the stack by the time they've got to
+# us. If we pass them on like this could mark us out as a bot.
 HEADER_MAP = {"Dnt": "DNT"}
 
+# Some values need to be faked or fixed
 HEADER_DEFAULTS = {
     # Mimic what it looks like if you got here from a Google search
     "Referer": "https://www.google.com",
@@ -37,8 +43,11 @@ HEADER_DEFAULTS = {
 }
 
 
-def clean_headers(headers, pass_cookies=False):
+def clean_headers(headers):
     """Remove Cloudflare and other cruft from the headers.
+
+    This will attempt to present a clean version of the headers which can be
+    used to make a request to the 3rd party site.
 
     Also:
 
@@ -50,16 +59,12 @@ def clean_headers(headers, pass_cookies=False):
     upstream service.
 
     :param headers: A mapping of header values
-    :param pass_cookies: Allow cookies through
     :return: An OrderedDict of cleaned headers
     """
     clean = OrderedDict()
 
     for header_name, value in headers.items():
         if header_name in BANNED_HEADERS:
-            continue
-
-        if header_name == "Cookie" and not pass_cookies:
             continue
 
         # Map to standard names for things
@@ -76,24 +81,40 @@ def clean_headers(headers, pass_cookies=False):
 def reorder_headers(target, exemplar):
     """Re-order the headers in the target to match an exemplar.
 
-    All fields in the exemplar will come first, other fields will appear in the
-    same order they were found at the end of the set of headers.
+    All fields in the exemplar will be re-ordered to match. Those not in the
+    exemplar will be left in place.
 
     :return: An OrderedDict of headers
     """
 
-    target_keys = list(target.keys())
-    exemplar_keys = list(exemplar.keys())
-    in_exemplar = set(target_keys) & set(exemplar_keys)
+    result = OrderedDict()
+    for key in _sort_list_by_exemplar(list(target.keys()), list(exemplar.keys())):
+        result[key] = target[key]
 
-    def sort_value(key):
-        if key in in_exemplar:
-            return 0, exemplar_keys.index(key)
+    return result
 
-        return 1, target_keys.index(key)
 
-    sorted_headers = OrderedDict()
-    for key in sorted(target_keys, key=sort_value):
-        sorted_headers[key] = target[key]
+def _sort_list_by_exemplar(target, exemplar):
+    result = [None] * len(target)
+    sortable_keys = []
+    available_indexes = []
 
-    return sorted_headers
+    for index, key in enumerate(target):
+        if key in exemplar:
+            # The exemplar has an opinion on ordering, so this position is
+            # available to be ordered, but we don't know which key will get it
+            sortable_keys.append(key)
+            available_indexes.append(index)
+        else:
+            # If it's not in the exemplar, we have no information about where
+            # to put it, so just put it back in place
+            result[index] = key
+
+    # All the sortable keys should be re-ordered into their exemplar order
+    sortable_keys.sort(key=exemplar.index)
+
+    # Then we can slot them back into the available indexes
+    for key, index in zip(sortable_keys, available_indexes):
+        result[index] = key
+
+    return result
